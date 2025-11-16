571183a00fe57bf6200f5fe6702c70dce7255647
a256b <arielb02965@gmail.com>
2025-11-16 13:55:56 -0300

    ui: sidebar

--- app/app.py
+++ app/app.py
@@ -3,10 +3,13 @@ import streamlit as st
 st.title("Trabajo Práctico Final 2025")
 st.header("Modelo de Predicción de Supervivencia en el Titanic")
 
-tab1, tab2, tab3 = st.tabs(["Análisis Exploratorio del Dataset",
-"Preprocesamiento de los Datos",
-"Modelado Predictivo"])
-tab1.write("TODO: tab1")
-tab2.write("TODO: tab2")
-tab3.write("TODO: tab3")
-
+with st.sidebar:
+    opcion = st.radio(
+        "Modelos",
+        [
+            "Árbol de decisión",
+            "Método de ensamble",
+            "Red neuronal artificial",
+            "Otro modelo"
+        ]
+    )
