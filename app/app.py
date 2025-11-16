930956542e0547926e2d24a7c08d5ccc7c304835
a256b <arielb02965@gmail.com>
2025-11-15 23:58:33 -0300

    feat: front WIP

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Trabajo Práctico Final 2025")
+st.header("Modelo de Predicción de Supervivencia en el Titanic")
+
+tab1, tab2, tab3 = st.tabs(["Análisis Exploratorio del Dataset",
+"Preprocesamiento de los Datos",
+"Modelado Predictivo"])
+tab1.write("TODO: tab1")
+tab2.write("TODO: tab2")
+tab3.write("TODO: tab3")
+
