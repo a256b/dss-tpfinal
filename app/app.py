953d822f32cc4dbdc45172f565042e82e607d0cb
ToroSantiago <santiagotoro89@gmail.com>
2025-11-23 14:48:55 -0300

    arbol decision finish

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import modelo_rna as rna
+import modelo_arbol as arbol
 
 st.title("Trabajo Práctico Final 2025")
 st.header("Modelo de Predicción de Supervivencia en el Titanic")
@@ -16,7 +17,7 @@ with st.sidebar:
     )
 
 if opcion=="Árbol de decisión":
-    pass # Reemplazar esta línea con la llamada a la predicción de este modelo
+    arbol.prediccion()
 
 if opcion=="Método de ensamble":
     pass # Reemplazar esta línea con la llamada a la predicción de este modelo
