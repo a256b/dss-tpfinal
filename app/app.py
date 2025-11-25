32fc4e9e594cfdcab6bfb5291540f5be7ec6ee95
luciana <lucianaaptw@gmail.com>
2025-11-25 07:19:44 -0300

    Resuelve conflictos de merge: mantiene preprocesamiento y elimina arbol decision

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import modelo_rna as rna
+import modelo_regresion_logistica as rl
+
 
 st.title("Trabajo Práctico Final 2025")
 st.header("Modelo de Predicción de Supervivencia en el Titanic")
@@ -11,7 +13,7 @@ with st.sidebar:
             "Árbol de decisión",
             "Método de ensamble",
             "Red neuronal artificial",
-            "Otro modelo"
+            "Regresión Logistica"
         ]
     )
 
@@ -24,5 +26,5 @@ if opcion=="Método de ensamble":
 if opcion=="Red neuronal artificial":
     rna.prediccion()
 
-if opcion=="Otro modelo":
-    pass # Reemplazar esta línea con la llamada a la predicción de este modelo
+if opcion=="Regresión Logistica":
+    rl.prediccion()
